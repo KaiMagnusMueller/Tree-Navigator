dc0e58ed4f695aa6e6e54f3e88bd321ecd54b654
Kai Magnus Müller <ndsignin@outlook.de>
2023-03-14 16:40:14 +0100

    Card CSS improvements
    - Fix card shadow getting cut off
    - Remove pointer events from image
    - Improve 2nd card gradient start color
    - Add fade transition for tutorial card
    - Fix tutorial navigation appearing when only one tutorial is visible

--- src/components/Tutorial.svelte
+++ src/components/Tutorial.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { tutorials } from '../stores';
 	import { IconButton, IconClose, IconForward, IconBack, Icon } from 'figma-plugin-ds-svelte';
-
+	import { fade } from 'svelte/transition';
 	export let viewedTutorials = undefined;
 
 	let visibleTutorials = $tutorials.filter((elem) => {
@@ -36,13 +36,13 @@
 			currentCard--;
 		}
 
-		console.log(document.getElementById(`tc${currentCard}`));
+		// console.log(document.getElementById(`tc${currentCard}`));
 		document.getElementById(`tc${currentCard}`).scrollIntoView({ behavior: 'smooth' });
 	}
 </script>
 
 {#if visibleTutorials.length > 0 && viewedTutorials}
-	<div class="tutorial--wrapper">
+	<div class="tutorial--wrapper" in:fade={{ duration: 100 }}>
 		<div class="tutorial--header">
 			<IconButton iconName={IconClose} on:click={handleCloseTutorial} />
 		</div>
@@ -51,9 +51,8 @@
 				{#each visibleTutorials as tutorial, index}
 					<div class="tutorial--card--wrapper" id="tc{index.toString()}">
 						<div class="tutorial--card">
-							<div class="card--bg">
-								<img src={tutorial.image} alt="" />
-							</div>
+							<div class="card--bg" />
+							<img src={tutorial.image} alt="" width="160" height="160" />
 							<div class="card--text">
 								<h3>
 									{tutorial.title}
@@ -66,23 +65,28 @@
 					</div>
 				{/each}
 			</div>
-			<div class="tutorial--navigation">
-				<div class="tutorial--btn--wrapper">
-					<div class="btn--area">
-						<IconButton
-							iconName={IconBack}
-							on:click={() => handleSwitchCard('prev')}
-							disabled={currentCard === 0 ? true : false} />
-					</div>
-					<div class="btn--area">
-						<!-- <Icon iconName={IconForward} /> -->
-						<IconButton
-							iconName={IconForward}
-							on:click={() => handleSwitchCard('next')}
-							disabled={currentCard >= visibleTutorials.length - 1 ? true : false} />
+
+			{#if visibleTutorials.length > 1}
+				<div class="tutorial--navigation">
+					<div class="tutorial--btn--wrapper">
+						<div class="btn--area">
+							<IconButton
+								iconName={IconBack}
+								on:click={() => handleSwitchCard('prev')}
+								disabled={currentCard === 0 ? true : false} />
+						</div>
+						<div class="btn--area">
+							<!-- <Icon iconName={IconForward} /> -->
+							<IconButton
+								iconName={IconForward}
+								on:click={() => handleSwitchCard('next')}
+								disabled={currentCard >= visibleTutorials.length - 1
+									? true
+									: false} />
+						</div>
 					</div>
 				</div>
-			</div>
+			{/if}
 		</div>
 	</div>
 {/if}
@@ -92,50 +96,21 @@
 		position: absolute;
 		bottom: 0px;
 		width: 100%;
-		padding: 8px 0;
 		background: linear-gradient(transparent, var(--figma-color-bg) 30%);
 	}
 	.tutorial--section {
 		position: relative;
 	}
 
+	.tutorial--section > div {
+		padding-top: 8px;
+		padding-bottom: 8px;
+	}
+
 	.tutorial--header {
 		display: flex;
 		flex-direction: row;
 		justify-content: end;
-	}
-
-	.tutorial--navigation {
-		position: absolute;
-		left: 0;
-		top: 0;
-		height: 100%;
-		width: 100%;
-		padding: 0 8px;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		pointer-events: none;
-	}
-
-	.tutorial--btn--wrapper {
-		width: 100%;
-		padding: 0 8px;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-	}
-
-	.btn--area {
-		pointer-events: auto;
-		opacity: 0;
-		padding: 4px;
-		border-radius: 50%;
-		background: var(--figma-color-bg);
-	}
-
-	.btn--area:hover {
-		opacity: 1;
 	}
 
 	.tutorial--scroller {
@@ -189,6 +164,8 @@
 	}
 
 	img {
+		pointer-events: none;
+
 		position: absolute;
 		right: 0;
 		top: 50%;
@@ -197,9 +174,9 @@
 		filter: drop-shadow(0px 2.697px 7.192px rgba(0, 0, 0, 0.25));
 		transform: rotate(9.16deg) translate(15%, -50%);
 	}
-
-	.tutorial--card::after {
-		border: 2px solid rgb(0 0 0 / 32%);
+	/* .tutorial--card::after */
+	.card--bg {
+		border: 2px solid rgb(0 0 0 / 20%);
 		border-radius: 10px;
 		top: 0;
 		left: 0;
@@ -211,12 +188,45 @@
 		pointer-events: none;
 	}
 
+	.tutorial--navigation {
+		position: absolute;
+		left: 0;
+		top: 0;
+		height: 100%;
+		width: 100%;
+		padding: 0 8px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		pointer-events: none;
+	}
+
+	.tutorial--btn--wrapper {
+		width: 100%;
+		padding: 0 8px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.btn--area {
+		pointer-events: auto;
+		opacity: 0;
+		padding: 4px;
+		border-radius: 50%;
+		background: var(--figma-color-bg);
+	}
+
+	.btn--area:hover {
+		opacity: 1;
+	}
+
 	.tutorial--card--wrapper:nth-of-type(3n + 1) .tutorial--card {
 		background: linear-gradient(80.04deg, hsl(202, 96%, 50%) 0%, hsl(220, 81%, 48%) 100%);
 	}
 
 	.tutorial--card--wrapper:nth-of-type(3n + 2) .tutorial--card {
-		background: linear-gradient(80.04deg, hsl(173, 96%, 50%) 0%, hsl(212, 82%, 48%) 100%);
+		background: linear-gradient(80.04deg, hsl(173, 96%, 40%) 0%, hsl(212, 82%, 48%) 100%);
 	}
 
 	.tutorial--card--wrapper:nth-of-type(3n + 3) .tutorial--card {
